9b2ca2b198535540fad6884ec3fe3b87887fc918
Mateusz <emnalyeriar@gmail.com>
2014-10-29 19:54:05 +0100

    added method for setting options, labels and drawing chart

--- chart.js
+++ chart.js
@@ -1,17 +1,30 @@
 (function(){
 
-    function Chart(selector, url) {
+    function ChartInit(selector, url, options) {
+        this.rawData = {};
         this.data = {};
-        this.time = {};
+        this.labels;
         this.datasets = [];
-        this.selector = selector
+        this.selector = selector;
+        this.options = {
+            scaleBeginAtZero: false,
+            bezierCurve: false,
+            responsive: true,
+            scaleBackdropPaddingY: 25,
+            scaleBackdropPaddingX: 25,
+            multiTooltipTemplate: "<%= datasetLabel %>: <%= value %>"
+        };
 
-        this.selector = document.getElementById(selector);
+        jQuery.extend(this.options, options);
+
         this.getData(url);
+        this.setLabels();
         this.createDatasets();
+        this.setData();
+        this.drawChart();
     }
 
-    Chart.prototype.getData = function(url) {
+    ChartInit.prototype.getData = function(url) {
         var chart = this;
         jQuery.ajax({
             url : url,
@@ -20,38 +33,56 @@
             success: function(returnedData) {
                 jQuery.each(returnedData, function(index, element) {
                     jQuery.each(element, function(key, value) {
-                        if (chart.data.hasOwnProperty(key) === false) {
-                            chart.data[key] = [];
+                        if (chart.rawData.hasOwnProperty(key) === false) {
+                            chart.rawData[key] = [];
                         }
-                        chart.data[key].push(value);
+                        chart.rawData[key].push(value);
                     })
                 });
             }
         });
     }
 
-    Chart.prototype.createDatasets = function() {
+    ChartInit.prototype.createDatasets = function() {
         var chart = this;
-        jQuery.each(chart.data, function(key, value) {
+        jQuery.each(chart.rawData, function(key, value) {
             chart.datasets.push({
-                label: key,
+                label: key.replace('paragons', ''),
                 fillColor: "rgba(83, 83, 83, 0.2)",
                 strokeColor: "rgb(114, 114, 114)",
                 pointColor: "rgb(208, 208, 208)",
                 pointStrokeColor: "#17222e",
                 pointHighlightFill: "#000",
                 pointHighlightStroke: "rgba(220,220,220,1)",
-                data: (chart.arraySum(chart.data[key]) === 0) ? null : chart.data[key]
+                data: (chart.arraySum(chart.rawData[key]) === 0) ? null : chart.rawData[key]
             })
         });
     }
 
-    Chart.prototype.arraySum = function (array) {
+    ChartInit.prototype.setLabels = function() {
+        this.labels = this.rawData.date;
+        delete this.rawData.date
+    }
+
+    ChartInit.prototype.setData = function() {
+        this.data = {
+            'labels': this.labels,
+            'datasets': this.datasets
+        };
+    }
+
+    ChartInit.prototype.drawChart = function() {
+        this.selector = document.getElementById(this.selector).getContext('2d');
+        new Chart(this.selector).Line(this.data, this.options);
+    }
+
+    ChartInit.prototype.arraySum = function (array) {
         return array.reduce(function(a, b) {
             return parseInt(a)+parseInt(b);
         });
     }
 
-    var chart = new Chart('paragon_chart', '');
+
+    //var chart = new ChartInit('', '');
 
 })()
